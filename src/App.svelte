edf6c669299b8168c7ab3ae8e97302d49a8c2f51
Desiree Lerma <desi.lerma96@yahoo.com>
2021-11-07 00:54:40 -0500

    Creates needed components. Adds img and alt text for hand washing steps in how to comp.

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,6 @@
+<script>
+import HowTo from "./HowTo.svelte";
+
+</script>
 <h1>Test</h1>
+<HowTo />
---
+++ src/HowTo.svelte
@@ -0,0 +1,2 @@
+<p>How To</p>
+<img src="https://www.who.int/gpsc/5may/How_To_HandWash_Poster.pdf" alt="Hand Washing Steps">
